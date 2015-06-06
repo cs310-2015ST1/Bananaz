f44ea21f2820ecb1a1884472aa0e22f73272e22e
Alex Gonzalez <the.alex@live.ca>
2015-06-06 16:43:00 -0700

    Fix bug preventing setting the integer-part of latitude and longitude

--- minion/garden/models.py
+++ minion/garden/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Because Django requires these
 MAX_LENGTH = 255
 DECIMAL_PLACES = 20
-MAX_DIGITS = 20
+MAX_DIGITS = 30 # For decimals, integer-part has max size of MAX_DIGITS - DECIMAL_PLACES
 
 class Garden(models.Model):
     latitude = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
