44c68d588823483680143d5f85a0240630e652d0
Parynaz Adamy <p.adamy@live.ca>
2015-06-12 15:37:51 -0700

    Added login, home, and logout urls

--- minion/garden/urls.py
+++ minion/garden/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
 		url(r'^$', views.index, name='index'),
+
+		#twitterauth; MAY HAVE TO BE IN THE OTHER URLS.PY 
+		url(r'^$', 'django_social_app.views.login'),
+    	url(r'^home/$', 'django_social_app.views.home'),
+    	url(r'^logout/$', 'django_social_app.views.logout'),
 	]
