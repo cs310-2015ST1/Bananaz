8d857994dc28107fe6fe2d4c239b063b4a146c7d
Parynaz Adamy <p.adamy@live.ca>
2015-06-12 15:49:17 -0700

    Changed the urls and it works Brought home.html out of garden template

--- minion/garden/urls.py
+++ minion/garden/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 		url(r'^$', views.index, name='index'),
 
 		#twitterauth; MAY HAVE TO BE IN THE OTHER URLS.PY 
-		url(r'^$', 'django_social_app.views.login'),
-    	url(r'^home/$', 'django_social_app.views.home'),
-    	url(r'^logout/$', 'django_social_app.views.logout'),
+		url(r'^$', views.login, name = 'login'),
+    	url(r'^home/$', views.home, name = 'home'),
+    	url(r'^logout/$', views.logout, name = 'logout'),
 	]
