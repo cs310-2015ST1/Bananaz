6f5d6d9fa12120ec0b3bc03ed2c73b5afb2603dc
Alex Gonzalez <the.alex@live.ca>
2015-06-13 12:45:17 -0700

    Remove unused twitter login urls and views

--- minion/garden/views.py
+++ minion/garden/views.py
@@ -1,13 +1,11 @@
-from django.shortcuts import render_to_response, redirect, render
+from django.shortcuts import redirect, render
 from django.contrib.auth import logout as auth_logout
-from django.contrib.auth.decorators import login_required
 from django.utils.datastructures import MultiValueDictKeyError
 
 from .models import Garden
 from .models import FoodTree
 
 
-# Create your views here.
 def index(request):
 	food_types = generate_food_types()
 	gardens = Garden.objects.order_by('name')
@@ -15,18 +13,6 @@ def index(request):
 
 
 # twitterauth
-def login(request):
-	# context = RequestContext(request, {
-	#     'request': request, 'user': request.user})
-	# return render_to_response('login.html', context_instance=context)
-	return render(request, 'login.html')
-
-
-@login_required(login_url='/')
-def home(request):
-	return render_to_response('home.html')
-
-
 def logout(request):
 	auth_logout(request)
 	return redirect('/')
