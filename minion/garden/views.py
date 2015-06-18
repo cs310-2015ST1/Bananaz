d8e9fd29c7e122fa08e0a1c6bf75d7b1d6a36f53
Alex Gonzalez <the.alex@live.ca>
2015-06-18 11:14:42 -0700

    Revert "Still writing tests for searching criteria"
    This reverts commit 534e2aa1cbe0813cea7ac62ef29a053395957efb.

--- minion/garden/views.py
+++ minion/garden/views.py
@@ -10,9 +10,6 @@ from django.core.context_processors import csrf
 from .models import Garden
 from .models import FoodTree
 
-global_var_garden = []
-
-
 def index(request):
 	food_types = generate_food_types()
 	gardens = Garden.objects.order_by('name')
@@ -38,11 +35,7 @@ def logout(request):
 
 
 def render_index(request, gardens, food_types):
-    global_var_garden = gardens
-
-    for g in global_var_garden:
-        print(g.name)
-    return render(request, 'garden/index.html',
+	return render(request, 'garden/index.html',
 				{'gardens': gardens, 'food_types': food_types})
 
 
