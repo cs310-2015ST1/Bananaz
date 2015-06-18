744d3db7b8fb8742f15215df5df249b3ebfbc36d
Alex Gonzalez <the.alex@live.ca>
2015-06-18 11:20:08 -0700

    Remove unneeded test

--- minion/garden/tests.py
+++ minion/garden/tests.py
@@ -1,14 +1,4 @@
 from django.test import TestCase
-from models import Garden
-from models import User
-from models import FoodTree
+from models import *
 
 # Create your tests here.
-
-class GardenMethodTests(TestCase):
-
-    def test_ensure_latlong_are_positive(self):
-        # ensure_latlong_are_positive should results True for Gardens where lat and long are zero or positive
-                cat = Garden(latitude = -100,longitude=-1, name="TestGarden")
-                cat.save()
-                self.assertEqual((cat.latitude >= 0), True)
