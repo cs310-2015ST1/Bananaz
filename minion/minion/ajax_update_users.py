f49eaa439f8b99e1e9fd9b6cc6a6c93d92a587a2
Haoran Yu <haleyyew@gmail.com>
2015-07-02 15:14:06 -0700

    Duplicated code from Import Garden files

--- minion/minion/ajax_update_users.py
+++ minion/minion/ajax_update_users.py
@@ -1,4 +1,6 @@
 __author__ = 'Haoran'
+from django.http import HttpResponse
+from garden.models import UserProfile
 
 def import_user_data(request):
     return False
--- minion/static/admin/update_users.js
+++ minion/static/admin/update_users.js
@@ -1,3 +1,34 @@
 /**
  * Created by Haoran on 2015-07-02.
  */
+$("#updateuser-form").submit(function(event) {
+    event.preventDefault();
+    clickedUpdate();
+});
+
+function setLog(r) {
+    $("#log").html(r);
+}
+function clickedUpdate() {
+    setLog("Please wait...");
+    $("#update-button").attr("disabled", "disabled");
+    $.ajax({
+        url: getImportUrl(),
+        type: "POST",
+        data: {
+            csrfmiddlewaretoken: getCsrfToken()
+        },
+        success: function() {
+            setLog("Success!");
+            $("#update-button").removeAttr("disabled");
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+
+            setLog("Error! " + errorThrown);
+            $("#update-button").removeAttr("disabled");
+        }
+    })
+}
